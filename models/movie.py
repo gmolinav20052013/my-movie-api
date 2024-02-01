2ee248ed77f8f1714b97431d735a37d6d042f9ab
Guillermo Molina <gmolinav2005@.com>
2024-02-01 14:46:06 -0600

    sqlAlchemi sqlite

---
+++ models/movie.py
@@ -0,0 +1,17 @@
+from config.database import Base
+from sqlalchemy import Column, Integer, String, Float
+
+class Movie(Base):
+    
+    __tablename__ = "movies"
+    
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    title = Column(String)
+    overview = Column(String)
+    year = Column(Integer)
+    rating = Column(Float)
+    categoria = Column(String)
+    
+    
+    
+     
